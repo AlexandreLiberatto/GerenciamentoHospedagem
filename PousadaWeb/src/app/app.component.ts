9ecb299ca5a27f7c9093da089b1f32fb9ca89514
Alexandre Liberato <alexandreliberatto@gmail.com>
2025-03-14 00:03:04 -0300

    refactor(routes): Corrigido erro de roteamento.
    Signed-off-by: Alexandre Liberato <alexandreliberatto@gmail.com>

--- PousadaWeb/src/app/app.component.ts
+++ PousadaWeb/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { NzLayoutModule } from 'ng-zorro-antd/layout'; // Importe o módulo de layout
 import { NzMenuModule } from 'ng-zorro-antd/menu'; // Importe o módulo de menu
 import { NzIconModule } from 'ng-zorro-antd/icon'; // Importe o módulo de ícones
@@ -8,6 +8,7 @@ import { NzIconModule } from 'ng-zorro-antd/icon'; // Importe o módulo de ícon
   selector: 'app-root',
   standalone: true,
   imports: [
+    RouterLink, 
     RouterOutlet,
     NzLayoutModule, // Adicione o módulo de layout
     NzMenuModule,   // Adicione o módulo de menu
