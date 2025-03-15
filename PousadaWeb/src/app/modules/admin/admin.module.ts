7dc84df944a4b805c1a9e453b14278e0ca882dba
Alexandre Liberato <alexandreliberatto@gmail.com>
2025-03-14 23:53:46 -0300

    feat(frontend): Criado página de cadastro de quartos.
    Signed-off-by: Alexandre Liberato <alexandreliberatto@gmail.com>

--- PousadaWeb/src/app/modules/admin/admin.module.ts
+++ PousadaWeb/src/app/modules/admin/admin.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { routes } from './admin.routes';
+import { DemoNgZorroAntdModule } from '../../DemoNgZorroAntdModule';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
+  declarations: [
+  ],
+
+  imports: [
+    RouterModule.forChild(routes),
+    DemoNgZorroAntdModule,
+    ReactiveFormsModule,
+    FormsModule
+  ],
 })
 export class AdminModule {}
--- PousadaWeb/src/app/modules/admin/components/post-room/post-room.component.ts
+++ PousadaWeb/src/app/modules/admin/components/post-room/post-room.component.ts
@@ -1,11 +1,40 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzFormModule } from 'ng-zorro-antd/form';
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { NzMessageService } from 'ng-zorro-antd/message';
 
 @Component({
   selector: 'app-post-room',
-  imports: [],
+  standalone: true,
+  imports: [
+    ReactiveFormsModule, // Adicione ReactiveFormsModule aqui
+    NzFormModule, // Para <nz-form>, <nz-form-item>, <nz-form-control>
+    NzInputModule, // Para <input nz-input>
+    NzButtonModule // Para <button nz-button>
+  ],
   templateUrl: './post-room.component.html',
-  styleUrl: './post-room.component.scss'
+  styleUrls: ['./post-room.component.scss']
 })
 export class PostRoomComponent {
+
+  roomDetailsForm: FormGroup;
+
+  constructor(private fb: FormBuilder,
+    private message: NzMessageService,
+    private router: Router
+  ){
+     this.roomDetailsForm = this.fb.group({
+      name: ['', Validators.required],
+      type: ['', Validators.required],
+      price: ['', Validators.required]
+     })
+  }
+
+  submitForm(){
+    //
+  }
 
 }
