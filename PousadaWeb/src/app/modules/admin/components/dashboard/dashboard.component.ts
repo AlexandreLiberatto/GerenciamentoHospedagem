4df32b0a4993d2398213bd1df5ff0b0c4c89eb2d
Alexandre Liberato <alexandreliberatto@gmail.com>
2025-03-15 16:26:25 -0300

    feat(frontend): Criado dashnboard
    Signed-off-by: Alexandre Liberato <alexandreliberatto@gmail.com>

--- PousadaWeb/src/app/modules/admin/components/dashboard/dashboard.component.ts
+++ PousadaWeb/src/app/modules/admin/components/dashboard/dashboard.component.ts
@@ -1,17 +1,38 @@
 import { Component } from '@angular/core';
 import { AdminService } from '../../admin-services/admin.service';
 import { NzMessageService } from 'ng-zorro-antd/message';
+import { NzAvatarModule } from 'ng-zorro-antd/avatar';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzCardModule } from 'ng-zorro-antd/card';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzSkeletonModule } from 'ng-zorro-antd/skeleton';
+import { CommonModule, NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [
+    NzButtonModule,
+    NzCardModule,
+    NzSkeletonModule,
+    NzAvatarModule,
+    NzIconModule,
+    CommonModule,
+    NgFor,
+    // outros módulos ou componentes
+  ],
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.scss'
+  styleUrl: './dashboard.component.scss',
+  standalone: true,
 })
 export class DashboardComponent {
 
+  //IMAGES
+  quarto1 = 'assets/img/quarto01.jpeg'
+
   currentPage = 1;
   rooms = [];
+  total:any;
+  loading = false;
 
   constructor(private adminService: AdminService,
     private message: NzMessageService,
@@ -22,7 +43,8 @@ export class DashboardComponent {
   getRooms(){
     this.adminService.getRooms(this.currentPage -1).subscribe(res => {
       console.log(res);
-      this.rooms = res;
+      this.rooms = res.roomDtoList;
+      this.total = res.totalPages * 1;
     })
   }
 
